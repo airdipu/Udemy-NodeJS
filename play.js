d65f36c8a04d281dc3095c046b9ef5298146f317
airdipu <airdipu@gmail.com>
2021-01-23 15:03:34 +0100

    added first javaScript file

---
+++ play.js
@@ -0,0 +1,22 @@
+const name = 'Abdur';
+let age = 28;
+const hobbis = true;
+age = 30;
+
+const summarizeUser = function(name, age, hobbis) {
+    return (
+        'name is ' +
+        name + 
+        ', age is ' +
+        age +
+        ' and the user hobbies is ' +
+        hobbis
+    );
+};
+
+const add = (a, b) => {
+    return a+ b;
+};
+console.log(add(1, 2));
+
+console.log(summarizeUser(name, age, hobbis));
